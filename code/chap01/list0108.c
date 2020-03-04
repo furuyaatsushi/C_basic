f47948aca7dd2818f2af5aab4cd06edf16dac9eb
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-04 23:50:35 +0900

    fix

--- code/chap01/list0108.c
+++ code/chap01/list0108.c
@@ -4,7 +4,7 @@ int main(void)
   int vx, vy;
   vx = 57;
   vy = vx + 10;
-  printf("vxの値は%nです。\n", vx);
-  printf("vyの値は%nです。\n", vy);
+  printf("vxの値は%dです。\n", vx);
+  printf("vyの値は%dです。\n", vy);
   return 0;
 }
