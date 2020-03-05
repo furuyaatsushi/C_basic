51da5c97942ef497479945348caa76248d8ac3c2
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-05 23:05:01 +0900

    Update list0114.c

--- code/chap01/list0114.c
+++ code/chap01/list0114.c
@@ -3,7 +3,7 @@ int main(void)
 {
   int n1, n2;
   int wa;
-  puts("二つの整数を入力してください。")
+  puts("二つの整数を入力してください。");
   printf("整数1："); scanf("%d", &n1);
   printf("整数2："); scanf("%d", &n2);
   wa = n1 + n2;
