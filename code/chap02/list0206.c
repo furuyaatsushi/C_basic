977414e96c5ef1230ca7fad2ae32030a9fedf906
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-06 23:59:49 +0900

    Create list0206.c

---
+++ code/chap02/list0206.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+  int n;
+  double x;
+  n = 9.99;
+  x = 9.99;
+  printf("int 型変数nの値：%d\n", n);
+  printf("n / 2：%d\n", n / 2);
+  printf("double 型変数xの値：%d\n", x);
+  printf("x / 2：%d\n", x / 2);
+  return 0;
+}
