2c823d22b8ac729dfe4dc58b4474f6b6561e027d
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-07 00:00:28 +0900

    fix

--- code/chap02/list0206.c
+++ code/chap02/list0206.c
@@ -8,6 +8,6 @@ int main(void)
   printf("int 型変数nの値：%d\n", n);
   printf("n / 2：%d\n", n / 2);
   printf("double 型変数xの値：%d\n", x);
-  printf("x / 2：%d\n", x / 2);
+  printf("x / 2：%f\n", x / 2);
   return 0;
 }
