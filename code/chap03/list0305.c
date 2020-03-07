bde559bf0ad0e1f6931645a81b74eda82c468560
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-08 01:14:05 +0900

    Create list0305.c

---
+++ code/chap03/list0305.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+  int num;
+  printf("整数を入力してください：");
+  scanf("%d", &num);
+  if(num)
+    puts("その数はゼロではありません。");
+  else
+    puts("その数はゼロです。");
+  return 0;
+}
