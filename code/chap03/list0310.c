a6c3a6e731fbd19322418d92eb0f8d280f661561
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-11 01:39:46 +0900

    Create list0310.c

---
+++ code/chap03/list0310.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+  int no;
+  printf("整数を入力してください：");
+  scanf("%d", &no);
+  if(no > 0)
+    if(no % 2 == 0)
+      puts("その数は偶数です。");
+    else
+      puts("その数は奇数です。");
+  else
+    puts("正でない値が入力されました。");
+
+  return 0;
+}
