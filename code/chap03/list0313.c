4dda08d7d491a21cd2860979876ed00bdf824cd2
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-11 23:38:38 +0900

    Update list0313.c

--- code/chap03/list0313.c
+++ code/chap03/list0313.c
@@ -4,7 +4,7 @@ int main(void)
   int n1, n2, n3, max;
   puts("三つの整数を入力してください。");
   printf("整数１："); scanf("%d", &n1);
-  printf("整数２："); scanf("%d", &n4);
+  printf("整数２："); scanf("%d", &n2);
   printf("整数３："); scanf("%d", &n3);
 
   max = n1;
