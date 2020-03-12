22c8e040ea5b2a29f340e26b313cdc887891d681
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-12 23:26:30 +0900

    Update list0314.c

---
+++ code/chap03/list0314.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+  int n1, n2, n3, max;
+  puts("三つの整数を入力してください。");
+  printf("整数１："); scanf("%d", &n1);
+  printf("整数２："); scanf("%d", &n2);
+  printf("整数３："); scanf("%d", &n3);
+
+  max = (n1 > n2) ? n1 : n2;
+  printf("大きい方の値は%dです。\n", max);
+  return 0;
+}
