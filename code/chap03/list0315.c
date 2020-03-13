405865d75c4e61500beb9ac4637118863e4a5e9e
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-13 23:50:22 +0900

    Update list0315.c

---
+++ code/chap03/list0315.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+  int n1, n2;
+  puts("二つの整数を入力してください。");
+  printf("整数１："); scanf("%d", &n1);
+  printf("整数２："); scanf("%d", &n2);
+  printf("それらの差は%dです。\n", (n1 > n2) ? n1 - n2 : n2 - n1);
+  return 0;
+}
