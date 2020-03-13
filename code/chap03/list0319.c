aa21de20531fd6eae9bbd54cf09b4685a44beb3c
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-14 03:11:40 +0900

    Create list0319.c

---
+++ code/chap03/list0319.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+  int no;
+  printf("整数：");
+  scanf("%d", &no);
+  switch(no % 3){
+    case 0 : puts("その数は3で割り切れます。"); break;
+    case 1 : puts("その数を3で割った剰余は1です。"); break;
+    case 2 : puts("その数を3で割った剰余は2です。"); break;
+  }
+  return 0;
+}
