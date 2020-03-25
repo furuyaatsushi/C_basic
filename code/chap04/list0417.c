0120025624f438a5dd910ab3069e207ab3c472a6
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-25 23:46:34 +0900

    Update list0417.c

--- code/chap04/list0417.c
+++ code/chap04/list0417.c
@@ -7,7 +7,7 @@ int main(void)
   printf("高さ："); scanf("%d", &height);
   printf("横幅："); scanf("%d", &width);
   for(i = 1; i <= height; i++){
-    for(j = 1; j <= width; i++)
+    for(j = 1; j <= width; j++)
       putchar('*');
     putchar('\n');
   }
