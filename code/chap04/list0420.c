bbe61bfd547ac5977a247ae59e04c6df51272a9a
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-27 00:43:23 +0900

    Update list0420.c

--- code/chap04/list0420.c
+++ code/chap04/list0420.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 int main(void)
 {
-  
+  int retry;
+  do{
+    int i, no;
+    do{
+      printf("正の整数を入力してください：");
+      scanf("%d", &no);
+      if(no <= 0)
+        puts("\a正でない数を入力しないでください。");
+    }while(no <= 0);
+
+    
+  }
 }
