e4f941491bd55e52956dc32ca86c3b64d3f017e1
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-26 23:41:16 +0900

    Update list0419.c

--- code/chap04/list0419.c
+++ code/chap04/list0419.c
@@ -8,7 +8,7 @@ int main(void)
   for(i = 1; i <= len; i++){
     for(j = 1; j <= len - i; j++)
       putchar(' ');
-    for(j = 1; j <= len; j++)
+    for(j = 1; j <= i; j++)
       putchar('*');
     putchar('\n');
   }
