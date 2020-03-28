878927bb61c5067ab73669c35c3165bd43f0feb3
furuyaatsushi <sdvxmnn@gmail.com>
2020-03-28 21:07:00 +0900

    0501

---
+++ code/chap05/list0501.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+  int uchida;
+  int satoh;
+  int sanaka;
+  int hiraki;
+  int masaki;
+  int sum = 0;
+
+  printf("5人の点数を入力してください。\n");
+  printf("１番："); scanf("%d", &uchida); sum += uchida;
+  printf("２番："); scanf("%d", &satoh); sum += satoh;
+  printf("３番："); scanf("%d", &sanaka); sum += sanaka;
+  printf("４番："); scanf("%d", &hiraki); sum += hiraki;
+  printf("５番："); scanf("%d", &masaki); sum += masaki;
+}
